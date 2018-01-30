e83860bfd462b3234c33397f05da91e9916dafb0
Hadyan Yusuf <hadyanyusuf@gmail.com>
2018-01-31 02:58:02 +0700

    Update PDF in Home Banner and Responsive for mobile in Service page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,10 +37,12 @@
         <div class="slider_container">
             <div id="myparallaxslider" class="parallaxslider">
                 <div class="slide">
-                    <div class="bgoverlay slider_bg slider_bg_1" data-order="1">
-                        <img src="img/slider/slider-1.jpg" class="img-responsive" id="desktop-only">
-                        <img src="img/slider/slider-1m.jpg" class="img-responsive" id="mobile-only">
-                    </div>
+                    <a href="#" download="img/slider/boston.pdf">
+                        <div class="bgoverlay slider_bg slider_bg_1" data-order="1">
+                            <img src="img/slider/slider-1.jpg" class="img-responsive" id="desktop-only">
+                            <img src="img/slider/slider-1m.jpg" class="img-responsive" download="img/slider/boston.pdf" id="mobile-only">
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
